298fd70baf568118b27ccae2b9551b591f7d8292
qlgr-hub <457709928@qq.com>
2025-02-09 20:04:08 +0800

    20250209

--- docker/ubuntu24.04/restart.sh
+++ docker/ubuntu24.04/restart.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker compose stop
-docker compose -p compiler_dev -f ./docker-compose.yml up -d  --build --force-recreate
+docker compose -p compiler_dev -f ./docker-compose.yml up -d # --build --force-recreate
