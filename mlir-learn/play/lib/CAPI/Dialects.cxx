bfdee7b498071684be50b9225747c91c4d6ab8be
qlgr-hub <457709928@qq.com>
2025-03-02 11:10:16 +0800

    20250302

--- mlir-learn/play/lib/CAPI/Dialects.cxx
+++
@@ -1,6 +0,0 @@
-#include "Play-c/Dialects.hxx"
-
-#include "Play/Play.hxx"
-#include "mlir/CAPI/Registration.h"
-
-MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(Play, play, mlir::play::PlayDialect)
--- mlir-learn/play/python/mlir_play/dialects/play_pybind11.py
+++
@@ -1,2 +0,0 @@
-from ._play_ops_gen import *
-from .._mlir_libs._playDialectsPybind11.play import *
